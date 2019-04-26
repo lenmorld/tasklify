d05f29a3475de296449222f4d18db48fd5776009
lenmorld <lenmorld@gmail.com>
2019-04-25 21:51:58 -0400

    prettier and multiple items

--- app/DragAndDrop.jsx
+++ app/DragAndDrop.jsx
@@ -1,45 +1,43 @@
 import React from "react";
 
 class DragAndDrop extends React.Component {
-
-	onDragStart = (event) => {
+	onDragStart = event => {
 		// get id of dragged element
 
 		console.log(`Dragging: ${event.target.id}`);
 		event.dataTransfer.setData("text", event.target.id);
-	}
+	};
 
 	onDragOver = event => {
-		event.preventDefault();	// default does not allow drop
-	}	
+		event.preventDefault(); // default does not allow drop
+	};
 
 	onDrop = event => {
 		event.preventDefault(); // default is open as link
 		const data = event.dataTransfer.getData("text");
-
 		console.log(`Dropping: ${data}`);
-	}
+	};
 
 	render() {
+		// TODO: implement passed props, or make it an HOC
+
 		return (
 			<div>
-			 {/* DROP HERE */}
-			<div onDragOver={this.onDragOver}
+				{/* DROP HERE */}
+				<div
+					onDragOver={this.onDragOver}
 					onDrop={this.onDrop}
-					style={{border: '1px solid blue', width: '300px', height: '300px'}}
-				>
-			</div>
+					style={{ border: "1px solid blue", width: "300px", height: "300px" }}
+				/>
 
-			{/* DRAG THIS */}
-			<div draggable="true"
-								id="1"
-								key={1}
-								onDragStart={this.onDragStart}>
-							{this.props.children}
-						</div>
+				{/* DRAG THESE */}
+				{[1, 2, 3, 4].map(item => (
+					<div draggable="true" id={item} key={item} onDragStart={this.onDragStart}>
+						{item}
+					</div>
+				))}
 			</div>
-
-		)
+		);
 	}
 }
 
