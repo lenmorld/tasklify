b247b22c600a5887e60ba8c87d03b5cc532c1f73
lenmorld <lenmorld@gmail.com>
2019-04-26 21:03:08 -0400

    implement withDragSource

--- app/DragAndDrop.jsx
+++ app/DragAndDrop.jsx
@@ -1,53 +1,61 @@
 import React from "react";
 
-class DragAndDrop extends React.Component {
-	onDragStart = event => {
-		// get id of dragged element
+/**
+ * props needed that must be passed by parent component
+ * - id : <Task id={1}>
+ */
 
-		console.log(`Dragging: ${event.target.id}`);
-		event.dataTransfer.setData("text", event.target.id);
-	};
+export const withDragSource = Component => {
+	class DragSource extends React.Component {
+		onDragStart = event => {
+			// get id of dragged element
+			console.log(`Dragging: ${event.target.id}`);
+			event.dataTransfer.setData("text", event.target.id);
+		};
 
-	onDragOver = event => {
-		event.preventDefault(); // default does not allow drop
-	};
+		render() {
+			const { id } = this.props;
 
-	onDrop = event => {
-		event.preventDefault(); // default is open as link
-		const data = event.dataTransfer.getData("text");
-		console.log(`Dropping: ${data}`);
-		// default HTML DnD is moving the element
-		// move to drop area
-		event.target.appendChild(document.querySelector(`#${data}`));
-	};
+			return (
+				<div draggable="true" id={id} onDragStart={this.onDragStart}>
+					<Component />
+				</div>
+			);
+		}
+	}
 
-	render() {
-		// TODO: implement passed props, or make it an HOC
+	return DragSource;
+};
 
-		return (
-			<div>
-				{/* DROP HERE */}
-				<div
-					onDragOver={this.onDragOver}
-					onDrop={this.onDrop}
-					style={{ border: "1px solid blue", width: "300px", height: "300px" }}
-				/>
+export const withDragAndDrop = Component => {
+	class DropTarget extends React.Component {
+		onDragOver = event => {
+			event.preventDefault(); // default does not allow drop
+		};
 
+		onDrop = event => {
+			event.preventDefault(); // default is open as link
+			const data = event.dataTransfer.getData("text");
+			console.log(`Dropping: ${data}`);
+			// default HTML DnD is moving the element
+			// move to drop area
+			event.target.appendChild(document.querySelector(`#${data}`));
+		};
+
+		render() {
+			// TODO: implement passed props, or make it an HOC
+
+			return (
 				<div
 					onDragOver={this.onDragOver}
 					onDrop={this.onDrop}
 					style={{ border: "1px solid blue", width: "300px", height: "300px" }}
 				>
-					{/* DRAG THESE */}
-					{["k1", "k2", "k3", "k4"].map(item => (
-						<div draggable="true" id={item} key={item} onDragStart={this.onDragStart}>
-							{item}
-						</div>
-					))}
+					<Component />
 				</div>
-			</div>
-		);
+			);
+		}
 	}
-}
 
-export default DragAndDrop;
+	return DropTarget;
+};
